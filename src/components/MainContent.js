b3857126f7d0b0f069c0bf7329e29b6ee9ee5940
15akash <40172763+15akash@users.noreply.github.com>
2022-10-05 17:36:51 +0530

    Add files via upload

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -10,7 +10,7 @@ const MainContent = () => {
     <div className="main-content-con">
         <div className='main-content'></div>
         <h1>India's Credit <br/>Revolution is here.<br/><span className='graditent-color-content'>Pay 1/3rd. </span>Anywhere.</h1>
-        <img src={cardLogo} className="card-image" alt='crd-image' />
+        <img src={cardLogo} className="card-image" alt='crd' />
         <form className='content-con-form'>
             <div className='phone-number-input'>
                 <input className='content-con-input' type='number' max='10' placeholder='Enter phone number' />
